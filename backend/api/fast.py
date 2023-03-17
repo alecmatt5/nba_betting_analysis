bfc422ffd7b60e45a6ab4149a2de882de0a21df5
Ricky Pastika <RickySC88@hotmail.com>
2023-03-17 08:50:17 +0700

    updating my files

--- backend/api/fast.py
+++ backend/api/fast.py
@@ -1,6 +1,7 @@
 from py_files.ngboost_team import get_y_pred_percentile_api
 from py_files.ngboost_team import load_ngboost_team_model
 from py_files.today_games_preprocessor import preprocess_advanced
+#from py_files.bet_scoring import *
 from pathlib import Path
 import pandas as pd
 from fastapi import FastAPI
@@ -9,11 +10,13 @@ from datetime import datetime
 import glob
 
 #today_date = datetime.today().strftime('%Y-%m-%d')
-today_date = glob.glob('data/pkl/demo*.pkl')[0]
+today_date = glob.glob('data/pkl/demo*.pkl')[-1]
+print(today_date)
 app = FastAPI()
 file_path_model = 'data/pkl/ngdemo.pkl'
 app.state.model = load_ngboost_team_model(file_path_model)
 app.state.df_app = pd.read_pickle(today_date).copy()
+#app.state.df_bets = get_today_betting_data('data/pkl/sbr_today_betting_data.pkl')
 # app.state.df_app, X_features = preprocess_advanced('boxscores_advanced_team_all.pkl',
 #                                         roll_methods=['mean', 'median', 'std'],
 #                                         ohe=True,
@@ -26,6 +29,19 @@ app.add_middleware(
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
+# @app.get("/predict")
+# def predict(percentile_target=0.54):
+#     percentile_target = float(percentile_target)
+#     bets = app.state.df_bets.copy()
+#     model = app.state.model
+#     df_app=app.state.df_app.copy()
+#     y_pred = get_y_pred_percentile_api(df_app, model,
+#                                             percentile=percentile_target)
+#     compare = merge_today_predictions_with_bet_data(y_pred=y_pred, bets=bets)
+#     predicted_bet = predict_today_bets(compare=compare)
+#     y_json = predicted_bet.to_dict()
+#     return y_json
+
 @app.get("/predict")
 def predict(percentile_target=0.54):
     percentile_target = float(percentile_target)
@@ -33,8 +49,7 @@ def predict(percentile_target=0.54):
     df_app=app.state.df_app.copy()
     y_pred = get_y_pred_percentile_api(df_app, model,
                                             percentile=percentile_target)
-    y_json = y_pred.to_dict()
-    return y_json
+    return y_pred
 
 
 @app.get("/")
@@ -44,7 +59,7 @@ def root():
 
 if __name__=='__main__':
     print(app.state.df_app)
-    y_json = predict(percentile_target=0.54)
-    print(app.state.df_app)
+    y_json = predict(percentile_target=0.6)
     print(y_json)
+    y_json.to_pickle('data/pkl/DEMO_PRED.pkl')
     #print(root)
